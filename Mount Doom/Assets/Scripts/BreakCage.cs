193168e5b850b4352fcf68d1c264330631f23aaf
LAPTOP-8PEB5H4U\minja <mta305@nyu.edu>
2016-11-17 00:12:07 -0500

    Added a few scripts

---
+++ Mount Doom/Assets/Scripts/BreakCage.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BreakCage : MonoBehaviour {
+    public GameObject goodGuy;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnCollisionEnter(Collision col)
+    {
+        if(col.gameObject.tag == "break")
+        {
+            goodGuy.GetComponent<Rigidbody>().useGravity = true;
+        }
+    }
+}
